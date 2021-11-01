d0cfa46398789285f1bcc6a2aa2141a8b5c4bfc8
Gustavo L F Walbon <gwalbon@linux.ibm.com>
2021-11-01 09:40:12 -0300

    Script to execute the test in a sequel based on benchmark list
    Signed-off-by: Gustavo L F Walbon <gwalbon@linux.ibm.com>

---
+++ per_tests_ppc64le.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+FILE_LIST="eigen_List_Of_Tests.txt"
+
+EIGEN="/root/gcc/objdir/tensorflow/bazel-out/*-opt/bin/tensorflow/core/kernels/eigen_benchmark_cpu_test
+--benchmark_out_format=json
+--benchmark_format=json
+"
+
+cat "${FILE_LIST}" | \
+while read -r line
+do
+	new_dir="results/$( echo ${line} | tr -d " ")"
+	mkdir -p "${new_dir}"
+	pushd "${new_dir}"
+	N=0
+	while [ $N -lt 10 ]
+	do
+			: Run benchmark turn $N
+			set -x
+			$EIGEN --benchmark_filter="${line}" \
+				--benchmark_out=eigen_benchmark_cpu_test-$(uname -m)_$(hostname	-s)-$N.json
+			set +x
+			N=$((N+1))
+	done
+	popd
+	sleep 1m
+done
+
