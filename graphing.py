04be6dd7f6bd7016244d198e61a4b38c35574e98
Gustavo L F Walbon <gwalbon@linux.ibm.com>
2022-03-28 20:28:42 -0300

    Graphing script update
    Reduce white space
    Relocate the legend
    Bar graphs instead
    
    Signed-off-by: Gustavo L F Walbon <gwalbon@linux.ibm.com>

--- graphing.py
+++ graphing.py
@@ -52,39 +52,58 @@ def main():
     for i in items:
         Yaxis[i] = dict()
 
-    once = True
     for f in jasons:
         Xaxis_items = []
-        count = 1
 
         for i in items:
             Yaxis[i][f] = []
 
-        for n in sorted(all_data[f].items(), key = lambda x:all_data[f][x[0]][base_x]):
+        count = 0
+        ordem = [na for na in sorted(all_data[f].items(), key = lambda x:all_data[f][x[0]][base_x])]
+        for n in ordem:
+            count+=1
             Xaxis_items.append(all_data[f][n[0]][base_x])
-            if once:
-                print(count, all_data[f][n[0]]['name'])
-                count+=1
+            print(count,all_data[f][n[0]]['name'].split("(")[1])
             for i in items:
                 Yaxis[i][f].append(all_data[f][n[0]][i])
-        once = False
 
     # graphs
     colors = ["#FF671F", "#0f62fe", "tab:green", "tab:brown","tab:red"]
 
     # Removing the items that are not valuable to have a graph based on the X values and names of
     # tests
-    items.remove(base_x)
     items.remove('name')
 
     if len(items) > 1:
-        fig, axis = plt.subplots(len(items), sharex=True)
+        fig, axis = plt.subplots(len(items), sharex=True, sharey=False)
+        fig.subplots_adjust(hspace=.2)
+        fig.subplots_adjust(wspace=.1)
+        fig.subplots_adjust(bottom=.1)
+        fig.subplots_adjust(left=.035)
+        fig.subplots_adjust(right=.990)
+        fig.subplots_adjust(top=.952)
+        legendado = []
         for i in items:
+            once = 0
             axis[items.index(i)].set_title(i.replace('_',' '))
             for n in range(len(jasons)):
-                axis[items.index(i)].plot(Xaxis_items, Yaxis[i][jasons[n]],
-                        color=colors[n],label=f"{jasons[n].split('-')[-1].split('.')[0]}")
-            legend = axis[0].legend(loc='upper right', shadow=True, fontsize='x-large')
+                axis[items.index(i)].bar(np.arange(1,len(Xaxis_items)+1)+once*.2,
+                                        Yaxis[i][jasons[n]],
+                                        color=colors[n],
+                                        label=f"{jasons[n].split('-')[-1].split('.')[0]}",
+                                        width=1/(1+len(jasons)))
+                axis[items.index(i)].set_xlim(-5,len(Xaxis_items)+5)
+                axis[items.index(i)].set_ylim(-min(Yaxis[i][jasons[n]])/20,max(Yaxis[i][jasons[n]])*1.20)
+
+                if once < len(jasons):
+                    once = once + 1
+                    if not jasons[n] in legendado:
+                        legend = fig.legend(loc='lower center', shadow=True, fontsize='x-large', ncol = 2)
+                        legendado.append(jasons[n])
+                if 'time' in i:
+                    axis[items.index(i)].set_ylabel("[seconds]")
+                if 'items_per_second' in i:
+                    axis[items.index(i)].set_ylabel("[calcs/second]")
     else:
         fig, axis = plt.subplots()
         for i in items:
@@ -92,9 +111,7 @@ def main():
             for n in range(len(jasons)):
                 axis.plot(Xaxis_items, Yaxis[i][jasons[n]],
                         color=colors[n],label=jasons[n].split("-")[-1].split(".")[0])
-                legend = axis.legend(loc='upper right', shadow=True, fontsize='x-large')
-
-    plt.xticks(Xaxis_items)
+                legend = fig.legend(loc='lower center', shadow=True, fontsize='x-large', ncol = 2)
 
     plt.show()
 
